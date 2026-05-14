01ad9c9830b8dd72c716b7c1f7cf7c745800bbec
Askia Islance Pesa <askiapesa@gmail.com>
2026-05-14 10:24:19 +0800

    dupes

--- src/features/TicketChat/ChatMessages.jsx
+++ src/features/TicketChat/ChatMessages.jsx
@@ -49,7 +49,6 @@ export default function ChatMessages({
   isImageFile,
   onOpenAttachment,
   onDownloadAttachment,
-  isImageFile,
   transcriptCreatorName,
 }) {
   const formatTimeAgo = (value) => {
